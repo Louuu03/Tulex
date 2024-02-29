e9a7a238ec36a38ba3d0820d345fcb1f02ad56c0
Lou <lou.yiw@gmail.com>
2024-02-29 21:05:38 +0800

    fix(component): header

--- src/components/commmon/header.tsx
+++ src/components/commmon/header.tsx
@@ -1,6 +1,6 @@
 // components/Header.tsx
 import { Flex, Text, Avatar, useMediaQuery, VStack } from '@chakra-ui/react';
-import { isNumber } from 'lodash';
+import { isEmpty, isNumber } from 'lodash';
 import { useRouter } from 'next/router';
 
 interface HeaderProps {
@@ -11,14 +11,24 @@ interface HeaderProps {
 const HeaderComponent: React.FC<HeaderProps> = ({ userName, userImage }) => {
   const { pathname } = useRouter();
   const [isLargerThan768] = useMediaQuery('(min-width: 768px)');
+  let parentTitle='';
 
   // Function to convert pathname like '/app/writings' to 'Writings'
   const formatTitle = (path: string) => {
     let returnTitle = '';
     const pathParts = path.split('/'); // Split the path into parts
     let lastPart = pathParts[pathParts.length - 1]; // Get the last part of the path
-    lastPart === '[id]' && (lastPart = pathParts[pathParts.length - 2]);
+    if(lastPart === '[id]'){
+      lastPart = pathParts[pathParts.length - 2];
+      parentTitle=pathParts[pathParts.length - 3];
+    }
     returnTitle = lastPart.charAt(0).toUpperCase() + lastPart.slice(1); // Capitalize the first letter
+    //get parent's title
+    if(returnTitle!=='App'&&returnTitle!=='Writing'&&returnTitle!=='Settings'&&returnTitle!=='Leaderboard'&&returnTitle!=='Speaking'){
+      isEmpty(parentTitle)&&(parentTitle=pathParts[pathParts.length - 2]);      
+    }
+
+    
     if (returnTitle === 'App') {
       returnTitle = 'Home';
     } else if (returnTitle === 'Allcategories') {
@@ -31,10 +41,15 @@ const HeaderComponent: React.FC<HeaderProps> = ({ userName, userImage }) => {
       returnTitle = 'All Events';
     } else if (returnTitle === 'Myevents') {
       returnTitle = 'My Events';
+    } else if (returnTitle === 'Learninglanguages') {
+      returnTitle = 'Learning Languages';
     }
+
+
     return returnTitle;
   };
   const title = formatTitle(pathname);
+ 
 
   return (
     <Flex
@@ -42,7 +57,8 @@ const HeaderComponent: React.FC<HeaderProps> = ({ userName, userImage }) => {
       w='100%'
       align='center'
       justify='space-between'
-      padding='5'
+      px='5'
+      pb='5px'
       paddingTop='2'
       mb={'5px'}
       position='fixed'
@@ -52,19 +68,20 @@ const HeaderComponent: React.FC<HeaderProps> = ({ userName, userImage }) => {
       zIndex='10'
       marginLeft={isLargerThan768 ? '65' : 'initial'}
       boxSize={'border-box'}
+      bg={'white'}
     >
       <VStack align={'left'} spacing={0}>
         <Text fontSize='sm' pt={'1'} mb={'-10px'}>
-          TULEX
+          TULEX{!isEmpty(parentTitle)&&' / '+parentTitle}
         </Text>
         <Text fontSize='4xl' fontWeight='bold'>
           {title}
         </Text>
       </VStack>
-      <Flex align='center'>
+     {title!=='User'&& <Flex align='center'>
         <Text marginRight='3'>{userName}</Text>
         <Avatar size='sm' name={userName} src={userImage} />
-      </Flex>
+      </Flex>}
     </Flex>
   );
 };
