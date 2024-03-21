d88d4882e9a4e26d224575aa56e63c3e2b74eb5b
Lou <lou.yiw@gmail.com>
2024-03-22 03:33:02 +0800

    add(api): add auth callback

---
+++ src/pages/api/auth/callback.ts
@@ -0,0 +1,65 @@
+//set tokens in cookies and check if user existed.
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { connectToDatabase } from '../../../../lib/mongodbClient';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Ensure method is POST
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+  // Parse the tokens sent from the frontend
+  let isNewUser = false;
+  const { idToken, accessToken, refreshToken, userId } = req.body;
+
+  // Check if the user existed
+
+  const { db } = await connectToDatabase();
+
+  // Check if the user exists
+  const user = await db.collection('users').findOne({ _id: userId });
+
+  if (!user) {
+    // New user, add them with file metadata
+    isNewUser = true;
+    const UserData = {
+      _id: userId,
+      plan: 0,
+      language: 0,
+      createdAt: new Date(),
+    };
+    await db.collection('users').insertOne(UserData);
+  } else {
+    // User exists, but no basic info
+    !user.birthday && (isNewUser = true);
+  }
+
+  // Cookie settings for development
+  const cookieOptions = {
+    secure: 'development',
+  };
+
+  // Set a reasonable expiry for each token
+  const idTokenExpiry = new Date(Date.now() + 300 * 1000); //  5 minutes
+  const accessTokenExpiry = new Date(Date.now() + 300 * 1000); //   5 minutes
+  const refreshTokenExpiry = new Date(Date.now() + 30 * 24 * 3600 * 1000); // 30 days
+  const userIdExpiry = new Date(Date.now() + 30 * 24 * 3600 * 1000); // 30 days
+
+  // Set cookies
+  res.setHeader('Set-Cookie', [
+    `isNewUser=${isNewUser}; Path=/; HttpOnly; SameSite=Strict; ${cookieOptions.secure ? 'Secure' : ''}`,
+    `userId=${userId}; Expires=${userIdExpiry.toUTCString()}; Path=/; HttpOnly; SameSite=Strict; ${cookieOptions.secure ? 'Secure' : ''}`,
+    `idToken=${idToken}; Expires=${idTokenExpiry.toUTCString()}; Path=/; HttpOnly; SameSite=Strict; ${cookieOptions.secure ? 'Secure' : ''}`,
+    `accessToken=${accessToken}; Expires=${accessTokenExpiry.toUTCString()}; Path=/; HttpOnly; SameSite=Strict; ${cookieOptions.secure ? 'Secure' : ''}`,
+    `refreshToken=${refreshToken}; Expires=${refreshTokenExpiry.toUTCString()}; Path=/; HttpOnly; SameSite=Strict; ${cookieOptions.secure ? 'Secure' : ''}`,
+  ]);
+
+  // Return a response to the client
+  isNewUser
+    ? res
+        .status(201)
+        .json({ message: 'User created and token stored in cookies' })
+    : res.status(200).json({ message: 'Tokens stored in cookies' });
+}
