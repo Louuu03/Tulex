fa8cadbe2ba67880ef049dd841edd1951d919915
Lou <lou.yiw@gmail.com>
2024-03-26 00:36:06 +0800

    fix(main): delete old usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,22 +5,16 @@ import '../styles/globals.scss';
 import HeaderComponent from '@/components/commmon/header';
 import Navbar from '@/components/commmon/navbar';
 import fakedata from '@/utils/fakedata';
-import { UserProvider } from '@/hooksAndContext/userContext';
-import { TokenProvider } from '@/hooksAndContext/tokenContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <TokenProvider>
-        <UserProvider>
           <HeaderComponent
             userName={fakedata.currentUser.name}
             userImage={fakedata.currentUser.image}
           />
           <Component {...pageProps} />
           <Navbar />
-        </UserProvider>
-      </TokenProvider>
     </ChakraProvider>
   );
 }
